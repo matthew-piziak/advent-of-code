da09285256611411dfc254ad58538d34baa3e45a
Matthew Piziak <matthew.piziak@gmail.com>
2023-12-01 15:11:54 -0500

    use transducers

--- 2023/day01.clj
+++ 2023/day01.clj
@@ -2,12 +2,15 @@
   (:require [aoc.utils :as u]
             [clojure.string :as s]))
 
-(defn ->cal [line]
+(defn cal [line]
   (let [digits (re-seq #"\d" line)]
     (str (first digits) (last digits))))
 
+(defn sum-lines [input f]
+  (transduce (map f) + 0 (u/read-lines input)))
+
 (defn part-one [input]
-  (->> input u/read-lines (map (comp #(Integer/parseInt %) ->cal)) (apply +)))
+  (sum-lines input (comp #(Integer/parseInt %) cal)))
 
 ;;; ex 01-1: 142
 (part-one "example01")
@@ -20,18 +23,21 @@
 
 (def digit-words (into {} (map (juxt to-english identity) (range 1 10))))
 
-(def re-digit (re-pattern (s/join "|" (conj (keys digit-words) "\\d"))))
+(defn re-disj [ms]
+  (re-pattern (s/join "|" ms)))
+
+(def re-digit (re-disj (conj (keys digit-words) "\\d")))
 
 ;;; avoid overlapping matches by just reversing the regex and reading from the end
-(def re-digit-reversed (re-pattern (s/join "|" (conj (map s/reverse (keys digit-words)) "\\d"))))
+(def re-digit-reversed (re-disj (conj (map s/reverse (keys digit-words)) "\\d")))
 
-(defn ->cal2 [line]
+(defn cal2 [line]
   (let [f (re-find re-digit line)
         l (s/reverse (re-find re-digit-reversed (s/reverse line)))]
     (str (or (digit-words f) f) (or (digit-words l) l))))
 
 (defn part-two [input]
-  (->> input u/read-lines (map (comp #(Integer/parseInt %) ->cal2)) (apply +)))
+  (sum-lines input (comp #(Integer/parseInt %) cal2)))
 
 ;;; ex 01-2: 281
 (part-two "large01")
